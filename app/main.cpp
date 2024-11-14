6e64c9c0fb4c56e4410e60956ca9d1d3d1e1d469
Leonardo Fadul <lsffadul@gmail.com>
2024-11-14 15:05:12 -0300

    WIP: app folder

---
+++ app/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "joj/logger.h"
+#include "joj/platform/win32/window_win32.h"
+
+int main()
+{
+    i32 a = 5;
+    std::cout << "a = " << a << std::endl;
+
+    win32_print();
+
+    std::cout << "Hello, Joj!" << std::endl;
+
+    JINFO("Hello, Joj!");
+    JDEBUG("Hello, Joj!");
+    JWARN("Hello, Joj!");
+    JERROR(joj::ErrorCode::FAILED, "Hello, Joj!");
+    JFATAL(joj::ErrorCode::FAILED, "Hello, Joj!");
+
+    return 0;
+}
