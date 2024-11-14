c92aee4e550f3fc3d2f141511596150bc1c902af
Leonardo Fadul <lsffadul@gmail.com>
2024-11-14 16:21:11 -0300

    File window_win32.h and using it in main

--- include/joj/platform/win32/window_win32.h
+++ include/joj/platform/win32/window_win32.h
@@ -1,6 +1,14 @@
 #ifndef JOJ_WIN32_WINDOW_H
 #define JOJ_WIN32_WINDOW_H
 
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
 
+#include <iostream>
+
+JINLINE void win32_print()
+{
+    std::cout << "Print from WIN32" << std::endl;
+}
 
 #endif // JOJ_WIN32_WINDOW_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "joj/defines.h"
 #include "joj/logger.h"
+#include "joj/platform/win32/window_win32.h"
 
 int main()
 {
     i32 a = 5;
     std::cout << "a = " << a << std::endl;
+
+    win32_print();
 
     std::cout << "Hello, Joj!" << std::endl;
 
