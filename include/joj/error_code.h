8e447792584e2e6216bc74cddf0d977231f82250
Leonardo Fadul <lsffadul@gmail.com>
2024-11-13 23:53:30 -0300

    Added comments

--- include/joj/error_code.h
+++ include/joj/error_code.h
@@ -1,3 +1,15 @@
+/**
+ * @file error_code.h
+ * @author Leonardo Fadul ( TODO: todo@domain.com)
+ * @brief This file contains the error codes used throughout the engine,
+ * and some helper functions for debug purposes.
+ * @version 0.1
+ * @date 2024-11-13
+ * 
+ * @copyright TODO: Copyright (c) 2024
+ * 
+ */
+
 #ifndef JOJ_ERROR_CODE_H
 #define JOJ_ERROR_CODE_H
 
@@ -6,13 +18,20 @@
 
 namespace joj
 {
+    /** @brief Represents the error codes for the engine. */
     enum class ErrorCode
     {
+        /** @brief OK code, used when the operation was successful. */
         OK,
+
+        /** @brief Generic error code, used when the operation failed. */
         FAILED,
     };
 
+    /** @brief Get the index of the ErrorCode */
     i32 err_to_int(ErrorCode err);
+
+    /** @brief Get the name of the ErrorCode */
     const char* err_to_str(ErrorCode err);
 }
 
