e3e22b955eda50255b71e1af800cdf4170948df0
Leonardo Fadul <lsffadul@gmail.com>
2024-11-13 22:56:23 -0300

    Using defines.h in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "defines.h"
 
 int main()
 {
+    i32 a = 5;
+    std::cout << "a = " << a << std::endl;
+
     std::cout << "Hello, Joj!" << std::endl;
+
     return 0;
 }
