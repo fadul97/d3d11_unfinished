1b3591145e07c02218defd6a89940f9077d9af4a
Leonardo Fadul <lsffadul@gmail.com>
2024-11-15 20:07:20 -0300

    D3D11Scene class

---
+++ include/joj/renderer/d3d11/scene_d3d11.h
@@ -0,0 +1,62 @@
+#ifndef JOJ_D3D11_SCENE_H
+#define JOJ_D3D11_SCENE_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_WINDOWS
+
+#include <vector>
+#include <memory>
+#include "renderable_object_d3d11.h"
+#include "systems/camera/free_camera.h"
+#include "constant_buffer_d3d11.h"
+
+namespace joj
+{
+    struct CBPerObject
+    {
+        joj::JFloat4x4 World = joj::float4x4_identity();
+    };
+
+    struct WorldCB
+    {
+        joj::JFloat4x4 wvp = joj::float4x4_identity();
+    };
+
+    class JAPI D3D11Scene
+    {
+    public:
+        D3D11Scene();
+        ~D3D11Scene();
+
+        D3D11Scene(const D3D11Scene&) = delete;
+        D3D11Scene& operator=(const D3D11Scene&) = delete;
+
+        u32 get_objects_count() const;
+
+        void add_renderable_object(std::unique_ptr<D3D11RenderableObject> obj);
+
+        void update(
+            const f32 dt,
+            const FreeCamera* camera,
+            const JFloat4x4 proj_float4x4,
+            D3D11ConstantBuffer& constant_buffer
+        );
+
+        void draw();
+
+    private:
+        std::vector<std::unique_ptr<D3D11RenderableObject>> m_objects;
+    };
+
+    inline u32 D3D11Scene::get_objects_count() const
+    { return m_objects.size(); }
+
+    inline void D3D11Scene::add_renderable_object(std::unique_ptr<D3D11RenderableObject> obj)
+    { m_objects.push_back(std::move(obj)); }
+}
+
+#endif // JPLATFORM_WINDOWS
+
+#endif // JOJ_D3D11_SCENE_H
---
+++ src/renderer/d3d11/scene_d3d11.cpp
@@ -0,0 +1,54 @@
+#include "renderer/d3d11/scene_d3d11.h"
+
+#if JPLATFORM_WINDOWS
+
+#include "engine.h"
+
+joj::D3D11Scene::D3D11Scene()
+{
+}
+
+joj::D3D11Scene::~D3D11Scene()
+{
+}
+
+void joj::D3D11Scene::update(
+	const f32 dt,
+	const FreeCamera* camera,
+	const JFloat4x4 proj_float4x4,
+	D3D11ConstantBuffer& constant_buffer)
+{
+	// Set constants
+	joj::JMatrix4x4 view = camera->get_view_mat();
+	joj::JMatrix4x4 proj = DirectX::XMLoadFloat4x4(&proj_float4x4);
+
+	for (const auto& obj : m_objects)
+	{
+		joj::JMatrix4x4 world = DirectX::XMLoadFloat4x4(&obj->get_world_float4x4());
+		joj::JMatrix4x4 wvp = world * view * proj;
+
+		CBPerObject cbPerObject;
+		XMStoreFloat4x4(&cbPerObject.World, XMMatrixTranspose(wvp));
+		constant_buffer.update(joj::Engine::s_renderer->get_device_context(), cbPerObject);
+
+		joj::Engine::s_renderer->get_device_context()->DrawIndexed(
+			obj->get_index_count(),
+			obj->get_index_location(),
+			obj->get_vertex_location()
+		);
+	}
+}
+
+void joj::D3D11Scene::draw()
+{
+	for (const auto& obj : m_objects)
+	{
+		joj::Engine::s_renderer->get_device_context()->DrawIndexed(
+			obj->get_index_count(),
+			obj->get_index_location(),
+			obj->get_vertex_location()
+		);
+	}
+}
+
+#endif // JPLATFORM_WINDOWS
