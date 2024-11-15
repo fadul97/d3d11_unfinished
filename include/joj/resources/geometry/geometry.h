79c7ae8171cc0129b832853192bf1381fd195c3c
Leonardo Fadul <lsffadul@gmail.com>
2024-11-15 11:20:41 -0300

    Geometry class

---
+++ include/joj/resources/geometry/geometry.h
@@ -0,0 +1,88 @@
+#ifndef JOJ_GEOMETRY_H
+#define JOJ_GEOMETRY_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#include "math/jmath.h"
+#include <vector>
+
+namespace joj
+{
+    enum class GeometryType { UNKNOWN, CUBE, CYLINDER, SPHERE, GEOSPHERE, GRID, QUAD };
+
+    class JAPI Geometry
+    {
+    public:
+        Geometry();                                         // construtor
+        virtual ~Geometry();                                // destrutor
+
+        virtual void translate(f32 dx, f32 dy, f32 dz);
+        virtual void move_to(f32 px, f32 py, f32 pz);
+
+        virtual JFloat3 get_position() const;
+
+        const GeometryVertex* get_vertex_data() const;
+        const u32* get_index_data() const;
+
+        u32 get_vertex_count() const;
+        u32 get_index_count() const;
+
+        std::vector<Vertex>& get_vertices();
+        std::vector<u32>& get_indices();
+
+        std::vector<u16>& get_indices16();    // TODO: Refactor
+
+    protected:
+        GeometryType m_type;
+
+        JFloat3 m_position;
+
+        std::vector<GeometryVertex> m_vertices;
+        std::vector<u32> m_indices;
+        std::vector<u16> m_indices16;    // TODO: Refactor
+
+        void subdivide();
+    };
+
+    inline void Geometry::translate(const f32 dx, const f32 dy, const f32 dz)
+    { m_position.x += dx; m_position.y += dy; m_position.z += dz; }
+
+    inline void Geometry::move_to(const f32 px, const f32 py, const f32 pz)
+    { m_position.x = px; m_position.y = py; m_position.z = pz; }
+
+    inline DirectX::XMFLOAT3 Geometry::get_position() const
+    { return m_position; }
+
+    inline const Vertex* Geometry::get_vertex_data() const
+    { return m_vertices.data(); }
+
+    inline const u32* Geometry::get_index_data() const
+    { return m_indices.data(); }
+
+    inline u32 Geometry::get_vertex_count() const
+    { return static_cast<u32>(m_vertices.size()); }
+
+    inline u32 Geometry::get_index_count() const
+    { return static_cast<u32>(m_indices.size()); }
+
+    inline std::vector<Vertex>& Geometry::get_vertices()
+    { return m_vertices; }
+
+    inline std::vector<u32>& Geometry::get_indices()
+    { return m_indices; }
+
+    inline std::vector<u16>& Geometry::get_indices16()
+    {
+        if (m_indices16.empty())
+        {
+            m_indices16.resize(m_indices.size());
+            for (size_t i = 0; i < m_indices.size(); ++i)
+                m_indices16[i] = static_cast<u16>(m_indices[i]);
+        }
+
+        return m_indices16;
+    }
+}
+
+#endif // JOJ_GEOMETRY_H
---
+++ src/resources/geometry/geometry.cpp
@@ -0,0 +1,75 @@
+#include "resources/geometry/geometry.h"
+
+#if JPLATFORM_WINDOWS
+
+joj::Geometry::Geometry()
+    : m_type(GeometryType::UNKNOWN)
+{
+}
+
+joj::Geometry::~Geometry()
+{
+}
+
+void joj::Geometry::subdivide()
+{
+    // using namespace DirectX;
+
+    // Save a copy of the original geometry
+    std::vector <Vertex> vertices_copy = m_vertices;
+    std::vector <u32> indices_copy = m_indices;
+
+    m_vertices.resize(0);
+    m_indices.resize(0);
+
+    //       v1
+    //       *
+    //      / \
+    //     /   \
+    //  m0*-----*m1
+    //   / \   / \
+    //  /   \ /   \
+    // *-----*-----*
+    // v0    m2     v2
+
+    u32 num_tris = static_cast<u32>(indices_copy.size() / 3);
+
+    for (u32 i = 0; i < num_tris; ++i)
+    {
+        Vertex v0 = vertices_copy[indices_copy[size_t(i) * 3 + 0]];
+        Vertex v1 = vertices_copy[indices_copy[size_t(i) * 3 + 1]];
+        Vertex v2 = vertices_copy[indices_copy[size_t(i) * 3 + 2]];
+
+        // Find center points of each edge
+        Vertex m0, m1, m2;
+        DirectX::XMStoreFloat3(&m0.pos, 0.5f * (DirectX::XMLoadFloat3(&v0.pos) + DirectX::XMLoadFloat3(&v1.pos)));
+        DirectX::XMStoreFloat3(&m1.pos, 0.5f * (DirectX::XMLoadFloat3(&v1.pos) + DirectX::XMLoadFloat3(&v2.pos)));
+        DirectX::XMStoreFloat3(&m2.pos, 0.5f * (DirectX::XMLoadFloat3(&v0.pos) + DirectX::XMLoadFloat3(&v2.pos)));
+
+        // Add new geometry
+        m_vertices.push_back(v0); // 0
+        m_vertices.push_back(v1); // 1
+        m_vertices.push_back(v2); // 2
+        m_vertices.push_back(m0); // 3
+        m_vertices.push_back(m1); // 4
+        m_vertices.push_back(m2); // 5
+
+        m_indices.push_back(i * 6 + 0);
+        m_indices.push_back(i * 6 + 3);
+        m_indices.push_back(i * 6 + 5);
+
+        m_indices.push_back(i * 6 + 3);
+        m_indices.push_back(i * 6 + 4);
+        m_indices.push_back(i * 6 + 5);
+
+        m_indices.push_back(i * 6 + 5);
+        m_indices.push_back(i * 6 + 4);
+        m_indices.push_back(i * 6 + 2);
+
+        m_indices.push_back(i * 6 + 3);
+        m_indices.push_back(i * 6 + 1);
+        m_indices.push_back(i * 6 + 4);
+    }
+}
+
+#endif // JPLATFORM_WINDOWS
