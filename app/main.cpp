87d331b14644b834cbda4e0616bab09fc46f55d3
Leonardo Fadul <lsffadul@gmail.com>
2024-11-16 12:37:54 -0300

    Using SkullDemo in main

--- app/main.cpp
+++ app/main.cpp
@@ -5,7 +5,7 @@
 #include "joj/platform/win32/timer_win32.h"
 #include "joj/renderer/d3d11/renderer_d3d11.h"
 #include "joj/jmacros.h"
-#include "hills_demo.h"
+#include "skull_demo.h"
 #include <sstream>
 #include <math/jmath.h>
 #include "joj/engine.h"
@@ -17,6 +17,6 @@ int main()
     joj::Engine engine;
     JOJ_LOG_IF_FAIL(engine.start());
 
-    HillsDemo app;
+    SkullDemo app;
     return engine.run(&app);
 }
