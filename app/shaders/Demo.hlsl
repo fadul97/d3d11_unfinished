6da4f3bb452b7ade20b4819ff9fdc68113ba4ea9
Leonardo Fadul <lsffadul@gmail.com>
2024-11-17 11:11:02 -0300

    Shder file for Demo apps

---
+++ app/shaders/Demo.hlsl
@@ -0,0 +1,117 @@
+//=============================================================================
+// Basic.fx by Frank Luna (C) 2011 All Rights Reserved.
+//
+// Basic effect that currently supports transformations, lighting, and texturing.
+//=============================================================================
+
+#include "LightHelper.hlsl"
+
+cbuffer cbPerObject : register(b0)
+{
+    float4x4 gWorld;
+    float4x4 gWorldInvTranspose;
+    float4x4 gWorldViewProj;
+    float4x4 gTexTransform;
+    Material gMaterial;
+}; 
+
+cbuffer cbPerFrame : register(b1)
+{
+    DirectionalLight gDirLights[3];
+    float3 gEyePosW;
+
+    int gLightCount;
+    
+    float gFogStart;
+    float gFogRange;
+    float4 gFogColor;
+};
+
+// Nonnumeric values cannot be added to a cbuffer.
+Texture2D gDiffuseMap;
+
+SamplerState samAnisotropic
+{
+    Filter = ANISOTROPIC;
+    MaxAnisotropy = 4;
+
+    AddressU = WRAP;
+    AddressV = WRAP;
+};
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float4 Color : COLOR;
+    float2 Tex : TEXCOORD;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float3 NormalW : NORMAL;
+    float4 Color : COLOR;
+};
+
+VertexOut VS(VertexIn vin)
+{
+    VertexOut vout;
+	
+	// Transform to world space space.
+    vout.PosW = mul(float4(vin.PosL, 1.0f), gWorld).xyz;
+    vout.NormalW = mul(vin.NormalL, (float3x3) gWorldInvTranspose);
+		
+	// Transform to homogeneous clip space.
+    vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
+    
+    // Set color
+    vout.Color = vin.Color;
+    
+    return vout;
+}
+ 
+float4 PS(VertexOut pin) : SV_Target
+{
+    // Interpolating normal can unnormalize it, so normalize it.
+    pin.NormalW = normalize(pin.NormalW);
+
+	// The toEye vector is used in lighting.
+    float3 toEye = gEyePosW - pin.PosW;
+
+	// Cache the distance to the eye from this surface point.
+    float distToEye = length(toEye);
+
+	// Normalize.
+    toEye /= distToEye;
+	
+	//
+	// Lighting.
+	//
+
+	// Start with a sum of zero. 
+    float4 ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
+    float4 spec = float4(0.0f, 0.0f, 0.0f, 0.0f);
+
+	// Sum the light contribution from each light source.  
+	[unroll]
+    for (int i = 0; i < gLightCount; ++i)
+    {
+        float4 A, D, S;
+        ComputeDirectionalLight(gMaterial, gDirLights[i], pin.NormalW, toEye,
+			A, D, S);
+
+        ambient += A;
+        diffuse += D;
+        spec += S;
+    }
+
+    float4 litColor = ambient + diffuse + spec;
+
+	// Common to take alpha from diffuse material.
+    litColor.a = gMaterial.Diffuse.a;
+
+    return litColor;
+}
