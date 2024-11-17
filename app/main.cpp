01fd98b57a4ce3c5d91e12476005651eb0a093e6
Leonardo Fadul <lsffadul@gmail.com>
2024-11-17 12:44:36 -0300

    Using CrateDemo app in main

--- app/main.cpp
+++ app/main.cpp
@@ -5,7 +5,7 @@
 #include "joj/platform/win32/timer_win32.h"
 #include "joj/renderer/d3d11/renderer_d3d11.h"
 #include "joj/jmacros.h"
-#include "demos/lit_skull_demo.h"
+#include "demos/crate_demo.h"
 #include <sstream>
 #include <math/jmath.h>
 #include "joj/engine.h"
@@ -17,6 +17,6 @@ int main()
     joj::Engine engine;
     JOJ_RETURN_INT_IF_FAIL(engine.start());
 
-    LitSkullDemo app;
+    CrateDemo app;
     return engine.run(&app);
 }
