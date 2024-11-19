c9ce60921e1dee8a2fe3a8b72dbd8c64a45eca70
Leonardo Fadul <lsffadul@gmail.com>
2024-11-19 12:14:49 -0300

    Shader file for MirrorDemo

--- app/shaders/Demo.hlsl
+++ app/shaders/Demo.hlsl
@@ -64,7 +64,7 @@ VertexOut VS(VertexIn vin)
 		
     // Transform to homogeneous clip space.
     vout.PosH = mul(float4(vin.PosL, 1.0f), gWorldViewProj);
-
+	
     // Output vertex attributes for interpolation across triangle.
     vout.Tex = mul(float4(vin.Tex, 0.0f, 1.0f), gTexTransform).xy;
 
@@ -79,73 +79,73 @@ float4 PS(VertexOut pin) : SV_Target
     // Interpolating normal can unnormalize it, so normalize it.
     pin.NormalW = normalize(pin.NormalW);
 
-	// The toEye vector is used in lighting.
+    // The toEye vector is used in lighting.
     float3 toEye = gEyePosW - pin.PosW;
 
-	// Cache the distance to the eye from this surface point.
+    // Cache the distance to the eye from this surface point.
     float distToEye = length(toEye);
 
-	// Normalize.
+    // Normalize.
     toEye /= distToEye;
-	
+
     // Default to multiplicative identity.
     float4 texColor = float4(1, 1, 1, 1);
     if (gUseTexure)
     {
-		// Sample texture.
+        // Sample texture.
         texColor = gDiffuseMap.Sample(samAnisotropic, pin.Tex);
 
         if (gAlphaClip)
         {
-			// Discard pixel if texture alpha < 0.1.  Note that we do this
-			// test as soon as possible so that we can potentially exit the shader 
-			// early, thereby skipping the rest of the shader code.
+            // Discard pixel if texture alpha < 0.1.  Note that we do this
+            // test as soon as possible so that we can potentially exit the shader 
+            // early, thereby skipping the rest of the shader code.
             clip(texColor.a - 0.1f);
         }
     }
-	 
-	//
-	// Lighting.
-	//
+
+    //
+    // Lighting.
+    //
 
     float4 litColor = texColor;
     if (gLightCount > 0)
     {
-		// Start with a sum of zero.
+        // Start with a sum of zero.
         float4 ambient = float4(0.0f, 0.0f, 0.0f, 0.0f);
         float4 diffuse = float4(0.0f, 0.0f, 0.0f, 0.0f);
         float4 spec = float4(0.0f, 0.0f, 0.0f, 0.0f);
 
-		// Sum the light contribution from each light source.  
-		[unroll]
+        // Sum the light contribution from each light source.  
+        [unroll]
         for (int i = 0; i < gLightCount; ++i)
         {
             float4 A, D, S;
             ComputeDirectionalLight(gMaterial, gDirLights[i], pin.NormalW, toEye,
-				A, D, S);
+            A, D, S);
 
             ambient += A;
             diffuse += D;
             spec += S;
         }
 
-		// Modulate with late add.
+        // Modulate with late add.
         litColor = texColor * (ambient + diffuse) + spec;
     }
 
-	//
-	// Fogging
-	//
+    //
+    // Fogging
+    //
 
     if (gFogEnabled)
     {
         float fogLerp = saturate((distToEye - gFogStart) / gFogRange);
 
-		// Blend the fog color and the lit color.
+        // Blend the fog color and the lit color.
         litColor = lerp(litColor, gFogColor, fogLerp);
     }
 
-	// Common to take alpha from diffuse material and texture.
+    // Common to take alpha from diffuse material and texture.
     litColor.a = gMaterial.Diffuse.a * texColor.a;
 
     return litColor;
