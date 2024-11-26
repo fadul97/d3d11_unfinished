8a4d64f3b7c1a2818670e30d2e9960795b8a0e1d
Leonardo Fadul <lsffadul@gmail.com>
2024-11-26 16:07:44 -0300

    D3D11Mesh class, D3D11Submesh struct and added them to jojD3D11Renderer

---
+++ include/joj/renderer/d3d11/mesh_d3d11.h
@@ -0,0 +1,87 @@
+#ifndef JOJ_D3D11_MESH_H
+#define JOJ_D3D11_MESH_H
+
+#define JOJ_ENGINE_IMPLEMENTATION
+#include "defines.h"
+
+#if JPLATFORM_WINDOWS
+
+#include <d3d11.h>
+#include <vector>
+#include "vertex_buffer_d3d11.h"
+#include "index_buffer_d3d11.h"
+#include "logger.h"
+
+namespace joj
+{
+    struct D3D11Submesh
+    {
+        D3D11Submesh()
+            : id(-1), vertex_start(0), vertex_count(0),
+            face_start(0), face_count(0)
+        {
+        }
+
+        u32 id;
+        u32 vertex_start;
+        u32 vertex_count;
+        u32 face_start;
+        u32 face_count;
+    };
+
+    class JAPI D3D11Mesh
+    {
+    public:
+        D3D11Mesh();
+        ~D3D11Mesh();
+
+        D3D11Mesh(const D3D11Mesh& rhs) = delete;
+        D3D11Mesh& operator=(const D3D11Mesh& rhs) = delete;
+
+        template<typename VertexType>
+        ErrorCode set_vertices(ID3D11Device* device, const VertexType* vertices, u32 count);
+
+        ErrorCode set_indices(ID3D11Device* device, const u16* indices, u32 count);
+
+        void set_submeshes(std::vector<D3D11Submesh>& submeshes);
+
+        void draw(ID3D11DeviceContext* dc, u32 submesh_id);
+
+    private:
+        D3D11VertexBuffer m_vb;
+        D3D11IndexBuffer m_ib;
+
+        DXGI_FORMAT m_index_buffer_format;    // Always 16-bit
+        u32 m_vertex_stride;
+
+        std::vector<D3D11Submesh> m_submeshes;
+    };
+
+    template<typename VertexType>
+    inline ErrorCode D3D11Mesh::set_vertices(ID3D11Device* device, const VertexType* vertices, u32 count)
+    {
+        if (m_vb.is_filled())
+            m_vb.cleanup();
+
+        m_vertex_stride = sizeof(VertexType);
+
+        m_vb.setup(D3D11_USAGE_IMMUTABLE, 0, sizeof(VertexType) * count, vertices);
+
+        if (device->CreateBuffer(
+            m_vb.get_buffer_desc(),
+            m_vb.get_subdata(),
+            &m_vb.get_buffer()) != S_OK)
+        {
+            JERROR(ErrorCode::ERR_RENDERER_D3D11_VERTEX_BUFFER_CREATION,
+                "Failed to create Vertex Buffer.");
+
+            return ErrorCode::ERR_RENDERER_D3D11_VERTEX_BUFFER_CREATION;
+        }
+
+        return ErrorCode::OK;
+    }
+}
+
+#endif // JPLATFORM_WINDOWS
+
+#endif // JOJ_D3D11_MESH_H
---
+++ src/renderer/d3d11/mesh_d3d11.cpp
@@ -0,0 +1,52 @@
+#include "renderer/d3d11/mesh_d3d11.h"
+
+#if JPLATFORM_WINDOWS
+
+joj::D3D11Mesh::D3D11Mesh()
+{
+    m_index_buffer_format = DXGI_FORMAT_R16_UINT;
+    m_vertex_stride = 0;
+}
+
+joj::D3D11Mesh::~D3D11Mesh()
+{
+
+}
+
+joj::ErrorCode joj::D3D11Mesh::set_indices(ID3D11Device* device, const u16* indices, u32 count)
+{
+    m_ib.setup(sizeof(u16) * count, indices);
+
+    if (device->CreateBuffer(
+        m_ib.get_buffer_desc(),
+        m_ib.get_subdata(),
+        &m_ib.get_buffer()) != S_OK)
+    {
+        JERROR(ErrorCode::ERR_RENDERER_D3D11_INDEX_BUFFER_CREATION,
+            "Failed to create Index Buffer.");
+
+        return ErrorCode::ERR_RENDERER_D3D11_INDEX_BUFFER_CREATION;
+    }
+
+    return ErrorCode::OK;
+}
+
+void joj::D3D11Mesh::set_submeshes(std::vector<D3D11Submesh>& submeshes)
+{
+    m_submeshes = submeshes;
+}
+
+void joj::D3D11Mesh::draw(ID3D11DeviceContext* dc, u32 submesh_id)
+{
+    u32 offset = 0;
+
+    dc->IASetVertexBuffers(0, 1, &m_vb.get_buffer(), &m_vertex_stride, &offset);
+    dc->IASetIndexBuffer(m_ib.get_buffer(), m_index_buffer_format, 0);
+
+    dc->DrawIndexed(
+        m_submeshes[submesh_id].face_count * 3,
+        m_submeshes[submesh_id].face_start * 3,
+        0);
+}
+
+#endif // JPLATFORM_WINDOWS
