0f9df39e1edf5396d69bba99b38292370d1b9904
Leonardo Fadul <lsffadul@gmail.com>
2024-11-27 19:10:03 -0300

    Using D3D11Renderer2D!

--- app/demos/test2d.cpp
+++ app/demos/test2d.cpp
@@ -12,6 +12,8 @@ void Test2D::update(const f32 dt)
 
 void Test2D::draw()
 {
+    joj::Engine::s_renderer->clear(1.0f, 0.0f, 1.0f, 1.0f);
+    joj::Engine::s_renderer->swap_buffers();
 }
 
 void Test2D::shutdown()
