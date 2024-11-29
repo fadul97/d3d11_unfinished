157355efafe24cd9bd4a116e6eeb978481a2b8ed
Leonardo Fadul <lsffadul@gmail.com>
2024-11-29 18:36:58 -0300

    Renamed static_assert macro

--- include/joj/defines.h
+++ include/joj/defines.h
@@ -61,52 +61,52 @@ typedef bool b8;
 /** @brief 32-bit boolean type */
 typedef int b32;
 
-/* STATIC_ASSERT macro */
+/* JSTATIC_ASSERT macro */
 #if defined(__GNUC__)
-#define STATIC_ASSERT static_assert
+#define JSTATIC_ASSERT static_assert
 #elif defined(_MSC_VER)
-#define STATIC_ASSERT static_assert
+#define JSTATIC_ASSERT static_assert
 #else
-#define STATIC_ASSERT _Static_assert
+#define JSTATIC_ASSERT _Static_assert
 #endif
 
 // Ensure all types are of the correct size.
 
 /** @brief Assert u8 to be 1 byte.*/
-STATIC_ASSERT(sizeof(u8) == 1, "Expected u8 to be 1 byte.");
+JSTATIC_ASSERT(sizeof(u8) == 1, "Expected u8 to be 1 byte.");
 
 /** @brief Assert u16 to be 2 bytes.*/
-STATIC_ASSERT(sizeof(u16) == 2, "Expected u16 to be 2 bytes.");
+JSTATIC_ASSERT(sizeof(u16) == 2, "Expected u16 to be 2 bytes.");
 
 /** @brief Assert u32 to be 4 bytes.*/
-STATIC_ASSERT(sizeof(u32) == 4, "Expected u32 to be 4 bytes.");
+JSTATIC_ASSERT(sizeof(u32) == 4, "Expected u32 to be 4 bytes.");
 
 /** @brief Assert u64 to be 8 bytes.*/
-STATIC_ASSERT(sizeof(u64) == 8, "Expected u64 to be 8 bytes.");
+JSTATIC_ASSERT(sizeof(u64) == 8, "Expected u64 to be 8 bytes.");
 
 /** @brief Assert i8 to be 1 byte.*/
-STATIC_ASSERT(sizeof(i8) == 1, "Expected i8 to be 1 byte.");
+JSTATIC_ASSERT(sizeof(i8) == 1, "Expected i8 to be 1 byte.");
 
 /** @brief Assert i16 to be 2 bytes.*/
-STATIC_ASSERT(sizeof(i16) == 2, "Expected i16 to be 2 bytes.");
+JSTATIC_ASSERT(sizeof(i16) == 2, "Expected i16 to be 2 bytes.");
 
 /** @brief Assert i32 to be 4 bytes.*/
-STATIC_ASSERT(sizeof(i32) == 4, "Expected i32 to be 4 bytes.");
+JSTATIC_ASSERT(sizeof(i32) == 4, "Expected i32 to be 4 bytes.");
 
 /** @brief Assert i64 to be 8 bytes.*/
-STATIC_ASSERT(sizeof(i64) == 8, "Expected i64 to be 8 bytes.");
+JSTATIC_ASSERT(sizeof(i64) == 8, "Expected i64 to be 8 bytes.");
 
 /** @brief Assert f32 to be 4 bytes.*/
-STATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
+JSTATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes.");
 
 /** @brief Assert f64 to be 8 bytes.*/
-STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
+JSTATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes.");
 
 /** @brief Assert b8 to be 1 bytes.*/
-STATIC_ASSERT(sizeof(b8) == 1, "Expected b8 to be 1 byte.");
+JSTATIC_ASSERT(sizeof(b8) == 1, "Expected b8 to be 1 byte.");
 
 /** @brief Assert b32 to be 4 bytes.*/
-STATIC_ASSERT(sizeof(b32) == 4, "Expected f64 to be 4 bytes.");
+JSTATIC_ASSERT(sizeof(b32) == 4, "Expected f64 to be 4 bytes.");
 
 // Platform detection
 
