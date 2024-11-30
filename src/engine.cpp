10c8f04a71724b7a640fc5bb21963c23e25bfe46
Leonardo Fadul <lsffadul@gmail.com>
2024-11-30 12:00:07 -0300

    Changed d3d11 renderer to 3D in engine

--- src/engine.cpp
+++ src/engine.cpp
@@ -10,7 +10,7 @@
 joj::Win32Window* joj::Engine::s_window = nullptr;
 joj::Win32Input* joj::Engine::s_input = nullptr;
 joj::Win32Timer* joj::Engine::s_timer = nullptr;
-joj::D3D11Renderer3D* joj::Engine::s_renderer = nullptr;
+joj::D3D11Renderer2D* joj::Engine::s_renderer = nullptr;
 f32 joj::Engine::s_frametime = 0.0f;
 b8 joj::Engine::s_paused = false;
 b8 joj::Engine::s_running = false;
@@ -25,7 +25,7 @@ joj::Engine::Engine()
 {
 	s_window = new Win32Window();
 	s_timer = new Win32Timer();
-	s_renderer = new D3D11Renderer3D();
+	s_renderer = new D3D11Renderer2D();
 }
 
 joj::Engine::~Engine()
